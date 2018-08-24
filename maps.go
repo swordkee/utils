0e0342a0133245ad0b666eac8f6822737264ef35
swordkee <swordkee@futurebots.cn>
2018-08-24 17:01:03 +0800

    ArrayMerge

--- maps.go
+++ maps.go
@@ -84,6 +84,24 @@ func ArrayFlip(d map[string]string) map[string]string {
 	}
 	return nd
 }
+func ArrayMerge(s ...[]interface{}) (slice []interface{}) {
+	switch len(s) {
+	case 0:
+		break
+	case 1:
+		slice = s[0]
+		break
+	default:
+		s1 := s[0]
+		s2 := ArrayMerge(s[1:]...) //...将数组元素打散
+		slice = make([]interface{}, len(s1)+len(s2))
+		copy(slice, s1)
+		copy(slice[len(s1):], s2)
+		break
+	}
+
+	return
+}
 
 // convert map to struct
 /*
